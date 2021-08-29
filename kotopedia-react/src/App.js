30e05121088c7eccc05aa24822cce130e17f7fe9
Kyohan <smith.khalyl@gmail.com>
2021-08-29 14:22:21 -0500

    router links and some wip css

--- kotopedia-react/src/App.js
+++ kotopedia-react/src/App.js
@@ -1,46 +1,74 @@
 import React, { Component } from 'react';
 import './App.css';
-import Kotodummies from './models/Kotodummy.js';
-import Words from './models/Words.js';
+import Kotodummies from './components/Kotodummy.js';
+import Words from './components/Words.js';
+import Landing from './components/Landing.js';
 import {
   Navbar,
-  NavbarBrand,
   Nav,
   NavItem,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
 } from "reactstrap";
 
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
-class App extends Component {
-  renderNavBar = () => {
-    return (
-      <Router>
-          <Navbar color = "light" light expand = "md">
-            <NavbarBrand href = "/">Kotopedia</NavbarBrand>
-            <Nav className = "mr-auto" navbar>
-              <NavItem>
-                <Link to = "/kotodummies">Kotodummies</Link>
-              </NavItem>
-              <NavItem>
-                <Link to = "/words">Words</Link>
-              </NavItem>
-            </Nav>
-          </Navbar>
-          <main>
-            <Route path = "/kotodummies" component = {Kotodummies}/>
-            <Route path = "/words" component = {Words} />
-          </main>
-      </Router>
-    );
-  };
+export default class App extends Component {
 
   render() {
     return (
       <main>
-        {this.renderNavBar()}
+        <TopBar/>
       </main>
     );
   }
 }
 
-export default App;
+const TopBar = () => {
+  return (
+    <Router>
+        <Navbar className = "sticky-top" color = "light" expand = "sm">
+          <div class = "nav-logo-link">
+            <Link to = "/">
+              <img href = "/" class = "nav-logo" src = "img/doughie_logo.svg" alt = "Kotopedia"/>
+            </Link>
+          </div>
+          <Nav className = "mr-auto" navbar>
+            <NavItem>
+              <Link to = "/kotodummies">Kotodummies</Link>
+            </NavItem>
+            <NavItem>
+              <Link to = "/words">Words</Link>
+            </NavItem>
+          </Nav>
+        </Navbar>
+        <main>
+          <Switch>
+            <Route path = "/" exact component = {Landing}/>
+            <Route path = "/kotodummies" component = {Kotodummies}/>
+            <Route path = "/words" component = {Words} />
+          </Switch>
+        </main>
+    </Router>
+  );
+};
+
+export const Search = (props) => {
+  return (
+    <div class = "search-field">
+      <InputGroup>
+        <InputGroupAddon addonType = "prepend">
+          <InputGroupText><i class = "bi bi-search"></i></InputGroupText>
+        </InputGroupAddon>
+        <Input placeholder = {props.placeholder} class = "search-field" name = "query" type = "text" value = {props.value} onChange = {props.onChange}/>
+      </InputGroup>
+    </div>
+  )
+}
+
+export function renderPersonalities(personality) {
+  const path = "img/" + personality + ".png";
+  return <img class = "personality" src = {path} alt = {personality}/>
+}
