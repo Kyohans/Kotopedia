797aefab78e2f63d504d8fe96ef2584fa7f7ce09
Kyohan <smith.khalyl@gmail.com>
2021-10-31 16:38:30 -0500

    new landing page, need to fix the about page a little though

--- kotopedia-react/src/components/Landing.js
+++ kotopedia-react/src/components/Landing.js
@@ -1,19 +1,52 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import { Tooltip} from "reactstrap";
 import '../App.css';
 
 export default class Landing extends Component {
   render() {
     return (
-      <main className = "content">
-        <div id = "landing-container">
+      <>
+        <div class = "landing-container">
           <p class = "landing-text-logo">Kotopedia</p>
           <div class = "landing-links">
             <a href = "/kotodummies">Kotodummies</a>
             <a href = "/words">Words</a>
-            <a href = "/">Contribute</a>
           </div>
         </div>
-      </main>
+      </>
     )
   }
+}
+
+export const About = () => {
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+  const [copied, setCopied] = useState(false);
+
+  const showTooltip = () => setTooltipOpen(!tooltipOpen);
+  const toggleTooltip = () => setCopied(!copied);
+
+  return (
+    <>
+      <div class = "about-container">
+        <div class = "about-page">
+          <h1 style = {{ fontSize: "40px", marginBottom: "2rem" }}>Welcome to Kotopedia!</h1>
+          <p style = {{ fontSize: "20px" }}>
+            Kotopedia is an encyclopedia for Kotodama Diary made by <a class = "about-links" href = "https://github.com/Kyohans">Kyohan</a>.
+            Huge thanks to <a class = "about-links" href = "https://www.reddit.com/user/stone_coldfoxx/">u/stone_coldfoxx</a> and <a class = "about-links" href = "https://www.reddit.com/user/dearestmina">u/dearestmina</a> from the <a class = "about-links" href = "https://www.reddit.com/r/KotoDamaDiary/">r/KotoDamaDiary</a> community for the spreadsheet of kotodummies and words!
+            Without them and the community's help in contributing to the sheet, this website would not have been made. Although, there is still a lot of missing words and kotodummies to add here.
+            If you have anything you'd like to add or correct, shoot me a message on 
+            <span id = "discord-link" onClick = {() => {
+              navigator.clipboard.writeText('Kyohan#6342');
+              toggleTooltip();
+            }}> Discord </span>
+            or <a class = "about-links" href = "https://twitter.com/ky0h4n">Twitter</a>!
+
+            <Tooltip placement = 'bottom' isOpen = {tooltipOpen} target = 'discord-link' toggle = {showTooltip}>
+              { copied ? "Copied to clipboard!" : "Kyohan#6342" }
+            </Tooltip>
+          </p>
+        </div>
+      </div>
+    </>
+  )
 }
